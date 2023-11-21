903a1404325229970e96efce9bfb6116b3e99730
Lambdax <91626975+OfficialLambdax@users.noreply.github.com>
2023-11-21 07:51:28 +0100

    fix

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -872,11 +872,11 @@ impl Server {
 						let playername = &self.clients[client_idx].info.as_ref().unwrap().username;
 						let packet_data = packet.data_as_string();
 						let contents: Vec<&str> = packet_data.split(":").collect();
-						if contents.len() != 3 {
+						if contents.len() < 3 {
 							error!("Message Error - Message from `{}` is of invalid format", &playername);
 							return Ok(());
 						}
-						if contents[1] != &self.clients[client_idx].info.as_ref().unwrap().username {
+						if contents[1] != playername {
 							error!("Message Error - `{}` is trying to send chat messages for another player `{}`", &playername, &contents[1]);
 							return Ok(());
 						}
